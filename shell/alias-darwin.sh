c99af8fa3974338d97f9634c064238975004b2a0
Vicen Dominguez <vicendominguez@users.noreply.github.com>
2019-05-17 10:04:29 +0200

    Update alias-darwin.sh

--- shell/alias-darwin.sh
+++ shell/alias-darwin.sh
@@ -9,4 +9,5 @@ alias valencia='curl -4 http://wttr.in/Valencia,Spain'
 alias rg='rg --no-ignore --hidden'
 alias oktap='aws-okta exec old_aws_production_super -- '
 alias oktas='aws-okta exec old_aws_staging_super -- '
+alias motherp="aws-okta exec old_aws_production_super -- aws ssm start-session --target i-0855982a811570154"
 alias jsonlintall='find . -name "*.json" -exec jsonlint -c -q {} \;'
