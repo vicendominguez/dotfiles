7f82f85e29f43fd2b38aa9981971e53c18b22758
Vicen Domínguez <vicendominguez@users.noreply.github.com>
2022-09-29 16:40:16 +0200

    Update alias-Darwin.sh
    new version

--- shell/alias-Darwin.sh
+++ shell/alias-Darwin.sh
@@ -7,13 +7,25 @@ alias ls='ls -G'
 alias myweather='curl -4 http://wttr.in/'
 alias valencia='curl -4 http://wttr.in/Valencia,Spain'
 alias rg='rg --no-ignore --hidden'
-# alias oktap='aws-okta --mfa-provider okta --mfa-factor-type push exec aws_production_super --'
-# alias oktas='aws-okta --mfa-provider okta --mfa-factor-type push exec aws_staging_super --'
-# alias sokta='aws-okta --mfa-provider okta --mfa-factor-type push exec simplee_production_super --'
-# alias mothers="aws-okta --mfa-provider okta --mfa-factor-type push exec aws_staging_super -- aws ssm start-session --target i-7c4db1f8"
-# alias motherp="aws-okta --mfa-provider okta --mfa-factor-type push exec aws_production_super -- aws ssm start-session --target i-00733fd71ed94e28b"\
 alias awss="aws --profile sso_platform_sta_admin"
 alias awsp='aws --profile sso_platform_pro_admin'
 alias ssmsession="aws ssm start-session --target "
 alias jsonlintall='find . -name "*.json" -exec jsonlint -c -q {} \;'
 alias instances='awless list instances'
+aws_sso () {
+        account=$1
+        case $account in
+                (sta) profile=sso_platform_sta_admin  ;;
+                (pro) profile=sso_platform_pro_admin  ;;
+                (pro_ro) profile=sso_platform_pro_adminro  ;;
+                (*) echo "No profile found bobo!"
+                        return 1 ;;
+        esac
+        export AWS_PROFILE=$profile
+        aws sts get-caller-identity > /dev/null
+        if [ $? -eq 0 ]
+        then
+                return 0
+        fi
+        aws sso login --profile $profile
+}
